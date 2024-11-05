c6365f3aa41deac4f4e8b061bf14ef8c4171f4e7
bhanuprasad911 <bhanuprasadchintu.170403@gmail.com>
2024-11-05 17:55:02 +0530

    day2

--- day1/index.js
+++ day1/index.js
@@ -1,3 +1,4 @@
+console.log("Hello world..!")
 let i = 10
 let j = 12
 let k = "10"
@@ -6,3 +7,4 @@ console.log(j+k)
 console.log(i==j)
 console.log(i==k)
 console.log(i===k)
+console.log("Bhanu")
---
+++ day2/index.js
@@ -0,0 +1,13 @@
+function geter(){
+var age1 = document.getElementById("age")
+if (age1>18){
+alert("You are an adult")
+}
+else if(age1>80){
+    alert("Invalid age")
+}
+else if (age<18){
+alert("You are a minor")
+}
+
+}
